5728ae26cbf0d96bcf25c4a67c72b96d2b7a39f4
Beast7729 <74852077+Beast7729@users.noreply.github.com>
2023-04-24 23:39:14 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+class Login {
+
+    public static void main(String args[]) {
+        String database = "Login.accdb";
+        String url = "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)}";
+
+    }
+}
